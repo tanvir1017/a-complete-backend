94c59b5bafed28eb8632fbbadc76c4327d9762a4
tanvir1017 <developer.tanvirhossain@gmail.com>
2023-12-18 21:13:53 +0600

    feat: 🎸 Completed the data modeling basic, MVC, Schema, & many

--- backend/models/hospital-managment/hospital.model.js
+++ backend/models/hospital-managment/hospital.model.js
@@ -1,5 +1,30 @@
 import mongoose from "mongoose";
 
-const hospitalRecordSchema = new mongoose.Schema({}, { timestamps: true });
+const hospitalRecordSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    addressLine1: {
+      type: String,
+      required: true,
+    },
+    addressLine2: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    pinCode: {
+      type: String,
+      required: true,
+    },
+    specializedIn: [{ type: String }],
+  },
+  { timestamps: true }
+);
 
 export const Hospital = mongoose.model("Hospital", hospitalRecordSchema);
