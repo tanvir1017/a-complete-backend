53bea0c920e0cf3d0280e5cedcc437459c594582
tanvir1017 <developer.tanvirhossain@gmail.com>
2023-12-18 21:59:49 +0600

    feat: 🎸 Begin with new fresh file for backend

--- backend/models/hospital-managment/doctor.model.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-const doctorSchema = new mongoose.Schema({}, { timestamps: true });
-
-export const Doctor = mongoose.model("Doctor", doctorSchema);
--- backend/models/hospital-managment/hospital.model.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-const hospitalRecordSchema = new mongoose.Schema({}, { timestamps: true });
-
-export const Hospital = mongoose.model("Hospital", hospitalRecordSchema);
