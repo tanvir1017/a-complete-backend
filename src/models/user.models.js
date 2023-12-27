c5865797ac124e6f31dfddce40e6d14fa8fc246c
tanvir1017 <developer.tanvirhossain@gmail.com>
2023-12-27 20:38:55 +0600

    working on models(user&video), creating accessToken & refreshToken

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,3 +1,5 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema(
@@ -46,5 +48,46 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+// Something to do before saving user data to db
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next(); // checking if password is not modifying then do not get into deeper, return from this line
+  this.password = bcrypt.hash(this.password, 10); // If password is modifying then hash it
+  next();
+});
+
+// checking is password is correct or not
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// generating access token
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+// generating refresh token
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
